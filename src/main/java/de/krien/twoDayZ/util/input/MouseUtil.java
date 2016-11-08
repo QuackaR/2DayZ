c1a568a5faf307dd76f85846d76263e8b5c17be9
Alex K <alexkrien@googlemail.com>
2016-11-08 17:20:37 +0100

    Fixed Context Menu Bug, but needs refactoring

--- src/main/java/de/krien/twoDayZ/util/input/MouseUtil.java
+++ src/main/java/de/krien/twoDayZ/util/input/MouseUtil.java
@@ -3,13 +3,13 @@ package de.krien.twoDayZ.util.input;
 import de.krien.twoDayZ.model.AGameEntity;
 import de.krien.twoDayZ.model.GameEntities;
 import de.krien.twoDayZ.model.menu.ContextMenu;
-import de.krien.twoDayZ.util.render.RenderContextMenuUtil;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector2f;
 
 public class MouseUtil {
 
+    //TODO Refactor
     public static void checkMouseActions() {
         while (Mouse.next()) {
             if (Mouse.getEventButtonState()) {
@@ -17,21 +17,20 @@ public class MouseUtil {
                     Vector2f eventPosition = new Vector2f(Mouse.getEventX(), Mouse.getEventY());
                     System.out.println("Left mouse button pressed at " + eventPosition.getX() + "/" + eventPosition.getY());
                 } else if (Mouse.getEventButton() == 1) {
-                    checkExistingMenu();
                     Vector2f eventPosition = new Vector2f(Mouse.getEventX(), Display.getHeight() - Mouse.getEventY());
-                    GameEntities.INSTANCE.getEntityList().add(new ContextMenu(eventPosition));
+                    boolean activeMenu = false;
+                    for (AGameEntity entity : GameEntities.INSTANCE.getEntityList()) {
+                        if (entity instanceof ContextMenu) {
+                            entity.setPosition(eventPosition);
+                            activeMenu = true;
+                        }
+                    }
+                    if (!activeMenu) {
+                        GameEntities.INSTANCE.getEntityList().add(new ContextMenu(eventPosition));
+                    }
                     System.out.println("Right mouse button pressed at " + eventPosition.getX() + "/" + eventPosition.getY());
                 }
             }
         }
     }
-
-    private static void checkExistingMenu() {
-        for(AGameEntity entity : GameEntities.INSTANCE.getEntityList()) {
-            if(entity instanceof ContextMenu) {
-                GameEntities.INSTANCE.getEntityList().remove(entity);
-            }
-        }
-    }
-
 }
